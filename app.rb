deeca4f6e2e7e8d7d58189a45b592b3389c5c261
hopelevin <hopealevin@gmail.com>
2018-08-15 18:35:52 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do
+    "Hello, my name is Hope! 😆"
+  end  
+  
+  get '/hometown' do
+    "My hometown is Huntington Station 🏘"
+  end
+  
+  get '/favorite_song' do
+    "My favorite song at the moment is Groupie Love by Lana Del Rey 🎶"
+  end
+  
+  get '/favorite_food' do
+    "My favorite food is sushi 🍣"
+    end
 end
